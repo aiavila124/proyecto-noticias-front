c98745607de00cea9570f4525cb85cf97e73abbd
Andrea Avila Parra <sistemas22@red5g.co>
2024-06-08 23:54:00 -0500

    pruebas_angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    // {path: '', component: AppComponent},
+    // {path: 'ruta2', component: LoginComponent}
+];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Persona } from './login.model';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -13,6 +15,14 @@ export class LoginComponent {
   mensaje = 'No se ha agregado ninguna persona';
   nombre = '';
   apellido = 'Apellido';
+  num1 = 0;
+  num2 = 0;
+  result: number = 0;
+  mostrar = true;
+
+  sumar(): void {
+    this.result = this.num1 + this.num2;
+  }
 
   checkDisabled() {
     if (this.mensaje == "Persona agregada"){
@@ -22,11 +32,25 @@ export class LoginComponent {
 
   agregarPersona() {
     this.mensaje = "Persona agregada";
+    this.mostrar = false;
   }
 
   modificarNombre(event:Event){
     this.nombre = (<HTMLInputElement>event.target).value; // Para obtener el valor del objeto html
     console.log("Evento:", this.nombre);
   }
+
+  nombreInput: string = '';
+  apellidoInput: string = '';
+
+  personas: Persona[] = [];
+
+  agregarPersona2() {
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    this.personas.push(persona1);
+    console.log(this.personas);
+
+  }
+
 
 }
